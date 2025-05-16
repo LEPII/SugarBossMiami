c80b6ddb8719563d49b60c63a49cbd88d93602e3
Luis Perez <lperezdev843@gmail.com>
2025-05-16 18:54:34 -0400

    simple footer

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Services from "./pages/Services";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/gallery" element={<Gallery />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
