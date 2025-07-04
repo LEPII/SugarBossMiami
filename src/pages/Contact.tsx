40e0857764c7809d364ea3213b31ae41bd5a2535
Luis Perez <lperezdev843@gmail.com>
2025-07-03 23:58:54 -0400

    stlying for navbar and form, still needs work

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,11 +1,18 @@
 import ContactForm from "../components/ContactForm";
+import ContactImage from "../assets/HeroPic.jpg";
+import "../style/pages/contactpage.css"
 
 const Contact = () => {
   return (
-    <>
-      <div>Let's Get In Touch</div>
-      <ContactForm />
-    </>
+    <div className="contact-page-wrapper">
+      <h1>Let's Get In Touch</h1>
+      <div className="image-form-container">
+        <img src={ContactImage} alt="Hero Pic" />
+        <div className="contact-form-overlay">
+          <ContactForm />
+        </div>
+      </div>
+    </div>
   );
 };
 
